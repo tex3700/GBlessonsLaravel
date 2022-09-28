a92a531f8b8c67ddf0f6c230b1c70b5d41c5ec2d
tex3700 <tex3700@gmail.com>
2022-09-28 09:57:52 +0300

    one correct file

--- resources/views/news/addNews.php
+++ resources/views/news/addNews.php
@@ -1,7 +1,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Авторизация</title>
+    <title>Добавление новости</title>
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css"
         rel="stylesheet"
